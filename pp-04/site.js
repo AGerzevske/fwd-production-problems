938c738b622f1a38b3238a191ed5823767459f35
Austyn Gerzevske <agerzevske@hawk.iit.edu>
2018-11-11 21:35:36 -0800

    Change html/css/js for more responsive js
      passed js linter, css linter w3, see note for html

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,26 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+// IIFE
+'use strict';
+(function() {
+  // Does browser support js?
+  if (typeof document.querySelector === 'undefined') {
+    // If no js, then return
+    return;
+  }
+  // console.log('browser supports java');
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // console.log('DOM is loaded');
+    var doorbelljs = document.querySelector('#doorbell');
+    var ring = document.querySelector('#ring');
+    var el = document.getElementById("ring");
+
+    doorbelljs.classList.toggle('no-visible');
+    ring.classList.toggle('clickable');
+    el.addEventListener("click", function ringDoorbell() {
+      // console.log('Click Event');
+      var doorbell = new Audio('media/doorbell.mp3');
+      doorbell.play();
+    }, false);
+
+  });
+}());
