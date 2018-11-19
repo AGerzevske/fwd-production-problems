c4b29a9ba4f8fd1a7dbe9563463b9a5d67d0ecbd
Austyn Gerzevske <agerzevske@hawk.iit.edu>
2018-11-18 16:01:15 -0800

    Add changes for exercise pp05

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,44 +2,57 @@
 //  outputs the correct value for x (5) even after double(6) is
 //  called:
 
-var x = 5;
+    var x = 5;
 
-function double(num) {
-  x = num * 2;
-  return x;
-}
+    var double = function(num) {
+      var x = num * 2;
+      return x;
+    }
 
-double(6);
-console.log('The value of x is:', x, 'It should be 5.');
+    double(6);
+    console.log('The value of x is:', x, 'It should be 5.');
+
 
 //  2. Rewrite the corrected JavaScript above as a self-executing
 //  anonymous function that does not pollute the global
 //  namespace (e.g., neither its variables nor the double
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
+var x = 5;
 
+console.log((function(num) {
+  'use strict';
+  var  x = num * 2;
+  return x;
+}(6)));
+
+
+console.log('The value of x is:', x, 'It should be 5.');
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:
-
-function arrayEach(array, func) {
-  for (i = 0; i < array.length; i++) {
+(function arrayEach(array, func) {
+  'use strict';
+  for (var i = 0; i < array.length; i++) {
     func(array[i]);
   }
-}
+}(['red','green','blue'], console.log));
 
-arrayEach(['red','green','blue'], console.log);
 
-console.log(i) // should be 'undefined', not 3
+console.log(typeof (i)); // should be 'undefined', not 3
 
 //  4. Explain why this function does not modify the global
 //  variable x declared on line 5 above. Write your explanation
 //  as JavaScript comments.
 
+// Because x in this case is givin a value from the argument 4 from addTwo(4).
+// In the function declaration the paramater 'x' is the name of the argument that was passed in
+// Since the paramater name has presedence over the global namespace it uses that x=4
+// The origional x stays the same because it was not altered in the function
 function addTwo(x) {
   x = x + 2;
   return x;
 }
 
-console.log(addTwo(4)); // 6
-console.log(x); // should be 5 if you corrected the double() function above
+console.log('should be 6',addTwo(4)); // 6
+console.log('should be 5',x); // should be 5 if you corrected the double() function above
